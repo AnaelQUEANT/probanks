f895fb29b8a776a315eb0c2f52eb026d8d1532f5
anaelQUEANT <queant.anael@gmail.com>
2024-10-21 09:18:37 +0200

    [Init] Spring boot initializr

---
+++ src/main/java/com/p2wbk/probanks/ProbanksApplication.java
@@ -0,0 +1,13 @@
+package com.p2wbk.probanks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbanksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbanksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p2wbk/probanks/ProbanksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p2wbk.probanks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbanksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
